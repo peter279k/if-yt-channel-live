361e4c8255ac9c9a59ad1722e4f02b218bbfdcb7
Peter <peter279k@gmail.com>
2023-10-26 01:16:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# if-yt-channel-live
+
+## Introduction
+
+- We use the IFTTT service to notify specific YouTube channel is live when running the `yt.py`.
+
+## Usage
+
+- Cloning the repository via the `git clone` command.
+- Using the `pipenv` to install dependencies.
+- Preparing the `.env` file and sample contents are as follows:
+
+```
+ifttt_service_url=your requested IFTTT webhook event service URL
+channel_name=The specific YouTube channel name
+```
+
+- It suggests that using the `cronjob` setting to let `yt.py` run as the scheduler.
---
+++ yt.py
@@ -0,0 +1,24 @@
+import requests
+from dotenv import dotenv_values
+
+
+config = dotenv_values('./.env')
+config_keys = list(config.keys())
+need_keys = [
+    'ifttt_service_url', 'channel_name',
+]
+for need_key in need_key:
+    if need_key not in config_keys:
+        raise Error('%s is not found in .env file!' % need_key)
+
+
+channel_name = config[need_keys[1]]
+channel_name_url = 'https://www.youtube.com/' + channel_name
+ifttt_event_url = config[need_keys[0]]
+
+if 'hqdefault_live.jpg' in response.text:
+    requests.get(ifttt_event_url)
+    print('Sending the %s is done!' % ifttt_event_url)
+
+
+print('Checking is done!')
