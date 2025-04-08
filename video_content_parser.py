51c130025b99db3fb3a54410e423d947b68dbc79
Peter <peter279k@gmail.com>
2025-04-08 16:57:12 +0800

    Fix logic about video content parsing/retrieving

--- video_content_parser.py
+++ video_content_parser.py
@@ -1,13 +1,34 @@
-import re
-from urllib.parse import urlparse
+import json
+from bs4 import BeautifulSoup
 
 
-def video_content_parser(video_link):
-    parsed = urlparse(video_link)
-    query = parsed.query
-    matched = re.findall(r'v=(\w+)', query)
+def video_content_parser(contents):
+    json_data_keyword = 'var ytInitialData = '
+    soup = BeautifulSoup(contents, 'html.parser')
+    scripts = soup.select('script')
+    json_content = None
+    for script in scripts:
+        if json_data_keyword in script.text:
+            json_content = script.text
+            break
+
+    if json_content is None:
+        print('Cannot find the JSON contents')
+        exit(0)
+
+
+    json_content = json_content.replace(json_data_keyword, '')
+    json_content = json_content[0:-1]
+
+    json_dict = json.loads(json_content)
     video_ids = []
-    if len(matched) == 1:
-        video_ids += matched[0],
+    try:
+        video_id = json_dict['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['videoActions']['menuRenderer']['topLevelButtons'][0]['segmentedLikeDislikeButtonViewModel']['likeButtonViewModel']['likeButtonViewModel']['toggleButtonViewModel']['toggleButtonViewModel']['defaultButtonViewModel']['buttonViewModel']['onTap']['serialCommand']['commands'][1]['innertubeCommand']['modalEndpoint']['modal]']['modalWithTitleAndButtonRenderer']['button']['buttonRenderer']['navigationEndpoint']['signInEndpoint']['nextEndpoint']['likeEndpoint']['target']['videoId']
+
+        video_ids = [video_id]
+        video_ids_append = video_ids.append
+     except Exception:
+        pass
+
 
     return video_ids
--- yt.py
+++ yt.py
@@ -20,30 +20,25 @@ channel_name_url = 'https://www.youtube.com/' + channel_name + '/live'
 ifttt_event_url = config[need_keys[0]]
 
 response = requests.get(channel_name_url)
-soup = BeautifulSoup(response.text, 'html.parser')
-video_link = soup.select('link[rel="canonical"]')
-if len(video_link) == 1:
-    video_link = video_link[0]['href']
-    print('Sending the %s is done!' % ifttt_event_url)
-    video_ids = video_content_parser.video_content_parser(video_link)
-    if len(video_ids) == 0:
-        print('There is no live video in this channel.')
-        exit(0)
+video_ids = video_content_parser.video_content_parser(response.text)
+if len(video_ids) == 0:
+    print('There is no live video in this channel.')
+    exit(0)
 
-    requests.get(ifttt_event_url)
-    yt_path = './yt_video_ids.txt'
-    contents = ''
-    if os.path.isfile(yt_path) is True:
-        with open(yt_path, encoding='utf-8', mode='r') as f:
-            contents = f.read()
+requests.get(ifttt_event_url)
+print('Sending the %s is done!' % ifttt_event_url)
+yt_path = './yt_video_ids.txt'
+contents = ''
+if os.path.isfile(yt_path) is True:
+    with open(yt_path, encoding='utf-8', mode='r') as f:
+        contents = f.read()
 
-    with open(yt_path, encoding='utf-8', mode='a') as f:
-        for video_id in video_ids:
-            if video_id in contents:
-                continue
+with open(yt_path, encoding='utf-8', mode='a') as f:
+    for video_id in video_ids:
+        if video_id in contents:
+            continue
 
-            f.write(video_id + '\n')
+        f.write(video_id + '\n')
 
-    print('The YouTube video id text file is saved.')
-
+print('The YouTube video id text file is saved.')
 print('Checking is done!')
